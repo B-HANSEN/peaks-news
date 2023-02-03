29cc6252f2a619513aab393196d0723a20471075
B-HANSEN <b.hansen.ubiqum@gmail.com>
2023-02-03 19:32:04 +0100

    setup application

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<h1>This will be my Peaks News App</h1>
+			</header>
+		</div>
+	);
+}
+
+export default App;
